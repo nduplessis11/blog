ad7976bc69fcc515bbe6b98dc0ba5ca535a25f6b
Nick <nduplessis11@gmail.com>
2024-07-15 13:34:24 -0400

    Clean up

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,42 +1,8 @@
-import fs from 'fs';
-import path from 'path';
-import matter from 'gray-matter';
-import Link from 'next/link';
-import { Post } from '../../types';
-
 export default function Blog() {
-  const posts = getPosts();
-
   return (
-    <div>
-      <h1>Blog</h1>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.slug}>
-            <Link href={`/blog/${post.slug}`}>
-              {post.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <main>
+      <h1>Welcome to my blog</h1>
+      <p>Welcome to my blog</p>
+    </main>
   );
-}
-
-function getPosts(): Post[] {
-  const postsDirectory = path.join(process.cwd(), 'public', 'posts');
-  const filenames = fs.readdirSync(postsDirectory);
-
-  return filenames.map((filename) => {
-    const filePath = path.join(postsDirectory, filename);
-    const fileContents = fs.readFileSync(filePath, 'utf8');
-    const { data, content } = matter(fileContents);
-
-    return {
-      slug: filename.replace(/\.md$/, ''),
-      title: data.title,
-      date: data.date,
-      content,
-    } as Post;
-  });
 }
